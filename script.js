8245cbfad33dd46a4556e77339c9ab643c394317
Ivan-Berezin <ibhol1702@gmail.com>
2023-04-27 11:32:19 -0600

    ADD Color function not finished

--- script.js
+++ script.js
@@ -31,14 +31,43 @@ while (t < num) {
 const boxes = document.querySelectorAll(".grid>.rows>div");
     for (const box of boxes) {
         box.classList.add("box");
-        box.addEventListener("mouseover", () => box.style.backgroundColor = "black");
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "red"; 
+            box.style.filter = "brightness(100%)";
+        });
     }
 
 }
  
+/* 
+    TO CREATE A RANDOM COLOR WHEN MOUSE OVER AND MAKE IT 10% DARKER EACH TIME MOUSE GOES THROUGH.
+
+    WHEN MOUSE GOES OVER A BLOCK
+        Function color() {
+            if (box.style.color = rgba(0,0,0,0) {
+        GENERATE RANDOM COLOR (This means that on mouseover, i need to run a function, called, set color, instead of just setting the color.)
+        SET BACKGROUND COLOR OF BLOCK TO RANDOM COLOR
+            }
+        Else (BLOCK ALREADY HAS COLOR) {
+            SET BRIGHTNESS TO -10%.
+        }
+    }
+
+*/
+
+
+ function color() {
+  if (box.style.backgroundColor = rgba(0,0,0,0)) {
+   box.style.backgroundColor = generateRandomColor();
+  } else {
+    box.style.filter = "brightness(" + (100 - 10) +"%)"; 
+  }
+}
+
+
+
 function removeChild() {
     const rows = document.querySelectorAll(".grid>div");
-    console.log(rows);
     for (row of rows) {
         row.remove();
     }
